63400cf150c61fc30d156bed8483c897423665a4
snklpp <sankalpkumar@Sankalps-MacBook-Air.local>
2026-05-10 22:46:35 +0530

    Context-aware options for vocab quiz and bump cache v10

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'jlpt-study-v9';
+const CACHE_NAME = 'jlpt-study-v10';
 const ASSETS = [
   './',
   './index.html',
-  './vocab.js?v=8',
-  './kanji.js?v=8',
+  './vocab.js?v=9',
+  './kanji.js?v=9',
   './icon-512.png'
 ];
 
