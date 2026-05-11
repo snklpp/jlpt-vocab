d3294d2a5a506bd422572e79b38978d24f6f7c40
snklpp <sankalpkumar@Sankalps-MacBook-Air.local>
2026-05-12 02:36:38 +0530

    Added Confident (Mastered) functionality for Kanji and Vocab and bumped cache to v21

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ const CACHE_NAME = 'jlpt-study-v20';
 const ASSETS = [
   './',
   './index.html',
-  './vocab.js?v=14',
-  './kanji.js?v=16',
+  './vocab.js?v=15',
+  './kanji.js?v=17',
   './grammar.js?v=1',
   './icon-512.png'
 ];
