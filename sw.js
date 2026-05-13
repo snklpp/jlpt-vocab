bde2821aef50d30539bd124c61d209dd00489605
snklpp <sankalpkumar@Sankalps-MacBook-Air.local>
2026-05-13 13:55:31 +0530

    Added word copy to clipboard when clicking specifically on the word

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'jlpt-study-v27';
+const CACHE_NAME = 'jlpt-study-v28';
 const ASSETS = [
   './',
   './index.html',
-  './vocab.js?v=27',
-  './kanji.js?v=27',
-  './grammar.js?v=27',
+  './vocab.js?v=28',
+  './kanji.js?v=28',
+  './grammar.js?v=28',
   './icon-512.png'
 ];
 
